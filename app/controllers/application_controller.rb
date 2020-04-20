8a99a02deba3c37aa14ec5f3f01f88706fd05db2
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-20 09:16:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,31 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-
+  
+  
+  get '/' do 
+    redirect '/recipes'
+  end 
+  
+  get '/recipes' do 
+    @recipes=Recipe.all 
+    erb :index
+  end 
+  
+  get '/recipes/new' do 
+    erb :new
+  end 
+  
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+  
+  get '/recipes/:id/edit' do 
+    
+  end 
+  
+ 
+  
+  
 end
